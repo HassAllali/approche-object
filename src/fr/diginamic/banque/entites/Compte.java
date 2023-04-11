1699c2b342c69ccd166614c2d7e67993e8977876
HassAllali <allalibouirig3@gmail.com>
2023-04-12 00:37:47 +0200

    Tp 5

---
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,19 @@
+package fr.diginamic.banque.entites;
+
+
+public class Compte {
+	
+	private String numeroCompte;
+	private double solde;
+	
+	Compte(String numeroCompte, double solde) {
+		this.numeroCompte = numeroCompte;
+		this.solde = solde;
+	}
+
+	@Override
+	public String toString() {
+		return "Votre numero de compte est " +  this.numeroCompte + ", et votre solde est " + solde;
+	}
+
+}
---
+++ src/fr/diginamic/banque/entites/TestBanque.java
@@ -0,0 +1,13 @@
+package fr.diginamic.banque.entites;
+import fr.diginamic.banque.entites.*;
+
+public class TestBanque {
+
+	public static void main(String[] args) {
+
+		Compte mycompte = new Compte("N°15", 1.574864867418678697481967896);
+		System.out.println(mycompte);
+
+	}
+
+}
