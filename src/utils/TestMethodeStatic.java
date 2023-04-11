e472d322295f6aab594952f2bc98062f5b90605d
HassAllali <allalibouirig3@gmail.com>
2023-04-12 00:11:35 +0200

    Tp 4

---
+++ src/utils/TestMethodeStatic.java
@@ -0,0 +1,17 @@
+package utils;
+import java.lang.Integer;
+
+public class TestMethodeStatic {
+
+	public static void main(String[] args) {
+	
+
+		String chaine = "12";
+		  int entier = Integer.parseInt(chaine);
+	       int a = 7;
+	       int b = 12;
+	       int max = Integer.max(a, b);
+	       System.out.println("Le nombre maximum entre " + a + " et " + b + " est " + max);
+	}
+
+}
